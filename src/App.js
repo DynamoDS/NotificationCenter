4907bbc5e40f8aea6aee1a2844c04a7cd8059c48
Aabishkar KC <aabishkar@gmail.com>
2022-08-09 12:01:07 -0400

    Add lint:fix as part of test script

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import NotificationsPanel from '@filipeop/notifications-panel';
 import Timestamp from '@hig/timestamp';
-import PropTypes from 'prop-types'
 
 function App() {
   let notificationData = [];
@@ -16,32 +15,32 @@ function App() {
       });
   }, []);
 
-    for (let i = 0; i < APIData.length; i++) {
-        var notificationItem = {
-            id: APIData[i].id,
-            featured: true,
-            unread: true,
-            message: APIData[i].title,
-            href: APIData[i].linkTitle,
-            timestamp: <Timestamp timestamp={APIData[i].created} />,
-            content: <div>
-                <a style={{ marginRight: '0.938rem' }}><img width="20" src={APIData[i].thumbnail} /></a>
-                <b>{APIData[i].title}</b>
-                <br /><br />
-                <p>{APIData[i].longDescription}</p>
-                <a href={APIData[i].link}>{APIData[i].linkTitle}</a>
-            </div>
-        };
-        notificationData.push(notificationItem);
-    }
+  for (let i = 0; i < APIData.length; i++) {
+    var notificationItem = {
+      id: APIData[i].id,
+      featured: true,
+      unread: true,
+      message: APIData[i].title,
+      href: APIData[i].linkTitle,
+      timestamp: <Timestamp timestamp={APIData[i].created} />,
+      content: <div>
+        <a style={{ marginRight: '0.938rem' }}><img width="20" src={APIData[i].thumbnail} /></a>
+        <b>{APIData[i].title}</b>
+        <br /><br />
+        <p>{APIData[i].longDescription}</p>
+        <a href={APIData[i].link}>{APIData[i].linkTitle}</a>
+      </div>
+    };
+    notificationData.push(notificationItem);
+  }
 
-    return (
-        <NotificationsPanel class="NotificationsFlyout"
-            heading="Notifications"
-            indicatorTitle="View application alerts"
-            notifications={notificationData}>
-        </NotificationsPanel>
-    );
+  return (
+    <NotificationsPanel class="NotificationsFlyout"
+      heading="Notifications"
+      indicatorTitle="View application alerts"
+      notifications={notificationData}>
+    </NotificationsPanel>
+  );
 }
 
 export default App;
