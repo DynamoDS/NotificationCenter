90e5ce2f6eee9414ba1843b177d4e1468a2c2899
Aaron (Qilong) <aaron.tang@autodesk.com>
2022-08-10 10:10:39 -0700

    use dev mode (#19)

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    mode: 'none',
+    mode: 'development',
     entry: {
         app: './src/index.js',
     },
