c84eb7233bdd266e95c0f27df0a720aff4fdd24a
Ashish Aggarwal <ashish.aggarwal@autodesk.com>
2022-08-17 12:20:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,34 +8,38 @@ function App() {
   const [APIData, setAPIData] = useState(false);
   useEffect(() => {
     window.RequestNotifications = RequestNotifications;
+    RequestNotifications();
   }, []);
   
   const RequestNotifications = (url) => {
     let getURL = url || process.env.NOTIFICATION_URL;
-    axios.get(getURL)
-    .then((response) => {
-      const notifications = response.data.notifications;
-      let notificationData = [];
-      for (let i = 0; i < notifications.length; i++) {
-        var notificationItem = {
-          id: notifications[i].id,
-          featured: true,
-          unread: true,
-          image: <img width={40} src={notifications[i].thumbnail}></img>,
-          message: notifications[i].title,
-          href: notifications[i].linkTitle,
-          timestamp: <Timestamp timestamp={notifications[i].created} />,
-          content: <div>
-            <b>{notifications[i].title}</b>
-            <p>{notifications[i].longDescription}</p>
-            <a href={notifications[i].link}>{notifications[i].linkTitle}</a>
-          </div>
-        };
-        notificationData.push(notificationItem);
-      }
-      setAPIData(notificationData);
-    });
+    if(getURL){
+      axios.get(getURL)
+      .then((response) => {
+        const notifications = response.data.notifications;
+        let notificationData = [];
+        for (let i = 0; i < notifications.length; i++) {
+          var notificationItem = {
+            id: notifications[i].id,
+            featured: true,
+            unread: true,
+            image: <img width={40} src={notifications[i].thumbnail}></img>,
+            message: notifications[i].title,
+            href: notifications[i].linkTitle,
+            timestamp: <Timestamp timestamp={notifications[i].created} />,
+            content: <div>
+              <b>{notifications[i].title}</b>
+              <p>{notifications[i].longDescription}</p>
+              <a href={notifications[i].link}>{notifications[i].linkTitle}</a>
+            </div>
+          };
+          notificationData.push(notificationItem);
+        }
+        setAPIData(notificationData);
+      });
+    }
   }
+
   return APIData ?
   <NotificationsPanel class="NotificationsFlyout"
       heading="Notifications"
