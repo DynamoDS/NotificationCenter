858f76a9d9d729781ba8744960837f1895f5456f
filipeotero <89042471+filipeotero@users.noreply.github.com>
2022-08-23 22:02:34 -0300

    adding the setNotifications function (#24)
    * adding the setNotifications function
    
    * update package version and notifictions-panel
    
    * removing console.log and extra import
    
    * using the URL parameter to request the backend
    
    * incrementing version

--- src/App.js
+++ src/App.js
@@ -1,51 +1,75 @@
 import './App.css';
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import NotificationsPanel from '@filipeop/notifications-panel';
+import NotificationsPanel from '@dynamods/notifications-panel';
 import Timestamp from '@hig/timestamp';
+import axios from 'axios';
 
 function App() {
-  const [APIData, setAPIData] = useState(false);
+
+  const [APIData, setAPIData] = useState({ loaded: false, notifications: [] });
+
+
   useEffect(() => {
-    window.RequestNotifications = RequestNotifications;
-    RequestNotifications();
-  }, []);
-  
-  const RequestNotifications = (url) => {
-    let getURL = url || process.env.NOTIFICATION_URL;
-    if(getURL){
-      axios.get(getURL)
-      .then((response) => {
-        const notifications = response.data.notifications;
-        let notificationData = [];
-        for (let i = 0; i < notifications.length; i++) {
-          var notificationItem = {
-            id: notifications[i].id,
-            featured: true,
-            unread: true,
-            image: <img width={40} src={notifications[i].thumbnail}></img>,
-            message: notifications[i].title,
-            href: notifications[i].linkTitle,
-            timestamp: <Timestamp timestamp={notifications[i].created} />,
-            content: <div>
-              <b>{notifications[i].title}</b>
-              <p>{notifications[i].longDescription}</p>
-              <a href={notifications[i].link} target="_blank">{notifications[i].linkTitle}</a>
-            </div>
-          };
-          notificationData.push(notificationItem);
-        }
-        setAPIData(notificationData);
-      });
+
+    if (process.env.NOTIFICATION_URL) {
+      axios.get(process.env.NOTIFICATION_URL)
+        .then((response) => {
+
+          let notificationsData = parseNotifications(response.data.notifications);
+          setAPIData(() => {
+            return {
+              loaded: true,
+              notifications: notificationsData
+            }
+          });
+
+        });
     }
+    else {
+      window.setNotifications = setNotifications;
+    }
+
+  }, []);
+
+  const setNotifications = (notifications) => {
+    let notificationsData = parseNotifications(notifications);
+    setAPIData(prevState => {
+      return {
+        loaded: true,
+        notifications: [...prevState.notifications, ...notificationsData]
+      }
+    });
   }
 
-  return APIData ?
-  <NotificationsPanel class="NotificationsFlyout"
+  function parseNotifications(notifications) {
+    let notificationsData = [];
+    for (let i = 0; i < notifications.length; i++) {
+      var notificationItem = {
+        id: notifications[i].id,
+        featured: true,
+        unread: true,
+        image: <img width={40} src={notifications[i].thumbnail}></img>,
+        message: notifications[i].title,
+        href: notifications[i].linkTitle,
+        timestamp: <Timestamp timestamp={notifications[i].created} />,
+        content: <div>
+          <b>{notifications[i].title}</b>
+          <p>{notifications[i].longDescription}</p>
+          <a href={notifications[i].link} target="_blank">{notifications[i].linkTitle}</a>
+        </div>
+      };
+      notificationsData.push(notificationItem);
+    }
+    return notificationsData;
+  };
+
+
+  return APIData.loaded ?
+    <NotificationsPanel class="NotificationsFlyout"
       heading="Notifications"
       indicatorTitle="View application alerts"
-      notifications={APIData}>
-  </NotificationsPanel>
-  : null;
+      notifications={APIData.notifications}>
+    </NotificationsPanel>
+    : null;
 }
 export default App;
