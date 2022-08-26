13189d963e28a2826db4250df48f7bb532706732
filipeotero <89042471+filipeotero@users.noreply.github.com>
2022-08-26 14:52:47 -0300

    mark all as read function subscription

--- src/App.js
+++ src/App.js
@@ -1,51 +1,69 @@
 import './App.css';
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import NotificationsPanel from '@filipeop/notifications-panel';
 import Timestamp from '@hig/timestamp';
 
 function App() {
-  const [APIData, setAPIData] = useState(false);
+  const [APIData, setAPIData] = useState({ loaded: false, notifications: [] });
   useEffect(() => {
-    window.RequestNotifications = RequestNotifications;
-    RequestNotifications();
+    window.setNotifications = setNotifications;
   }, []);
-  
-  const RequestNotifications = (url) => {
-    let getURL = url || process.env.NOTIFICATION_URL;
-    if(getURL){
-      axios.get(getURL)
-      .then((response) => {
-        const notifications = response.data.notifications;
-        let notificationData = [];
-        for (let i = 0; i < notifications.length; i++) {
-          var notificationItem = {
-            id: notifications[i].id,
-            featured: true,
-            unread: true,
-            image: <img width={40} src={notifications[i].thumbnail}></img>,
-            message: notifications[i].title,
-            href: notifications[i].linkTitle,
-            timestamp: <Timestamp timestamp={notifications[i].created} />,
-            content: <div>
-              <b>{notifications[i].title}</b>
-              <p>{notifications[i].longDescription}</p>
-              <a href={notifications[i].link} target="_blank">{notifications[i].linkTitle}</a>
-            </div>
-          };
-          notificationData.push(notificationItem);
-        }
-        setAPIData(notificationData);
-      });
+
+  const setNotifications = (notifications) => {
+    let notificationsData = [];
+    for (let i = 0; i < notifications.length; i++) {
+      var notificationItem = {
+        id: notifications[i].id,
+        featured: false,
+        unread: notifications[i].isUnread,
+        image: <img width={40} src={notifications[i].thumbnail}></img>,
+        message: notifications[i].title,
+        href: notifications[i].linkTitle,
+        timestamp: <Timestamp timestamp={notifications[i].created} />,
+        content: <div>
+          <b>{notifications[i].title}</b>
+          <p>{notifications[i].longDescription}</p>
+          <a href={notifications[i].link} target="_blank">{notifications[i].linkTitle}</a>
+        </div>
+      };
+      notificationsData.push(notificationItem);
+    }
+    setAPIData(prevState => {
+      return {
+        loaded: true,
+        notifications: [...prevState.notifications, ...notificationsData]
+      }
+    });
+
+
+  }
+
+  const markAllAsRead = () => {
+    let notificationsData = APIData.notifications;
+    for (let i = 0; i < notificationsData.length; i++) {
+      notificationsData[i].unread = false;
+    }
+
+    setAPIData(() => {
+      return {
+        loaded: true,
+        notifications: notificationsData
+      }
+    });
+
+    const readIds = notificationsData.map(x => x.id);
+    if (chrome.webview !== undefined) {
+      chrome.webview.hostObjects.scriptObject.SetNoficationsAsRead(readIds);
     }
   }
 
-  return APIData ?
-  <NotificationsPanel class="NotificationsFlyout"
+  return APIData.loaded ?
+    <NotificationsPanel class="NotificationsFlyout"
       heading="Notifications"
       indicatorTitle="View application alerts"
-      notifications={APIData}>
-  </NotificationsPanel>
-  : null;
+      onClickMarkAllAsRead={markAllAsRead}
+      notifications={APIData.notifications}>
+    </NotificationsPanel>
+    : null;
 }
 export default App;
